59f3d0aea811dac4a3ad65392cfffb9558d441e3
xcode73 <xcode.uln@gmail.com>
2022-01-18 17:57:02 +0400

    Initial Commit

---
+++ ToDoLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoLog
+//
+//  Created by Nikolai Eremenko on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
